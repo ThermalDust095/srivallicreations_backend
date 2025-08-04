a5ca941c5f5b242368aab7d51959d40a672597fb
ThermalDust095 <akhilpendyala69@gmail.com>
2025-08-04 15:11:04 +0530

    integrated postgres lezzzgooo

--- apiserver/ecommerce_backend/settings.py
+++ apiserver/ecommerce_backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'srivallicreations',
+        'USER': 'akhilp',
+        'PASSWORD': 'valli@1978',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def connection_status(request):
+    """
+    View to check the connection status.
+    """
+    return JsonResponse({"status": "connected"}, status=201)
