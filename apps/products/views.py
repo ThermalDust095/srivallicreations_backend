71126bcaabd8a623eceefccf957fb7287808f226
ThermalDust095 <akhilpendyala69@gmail.com>
2025-08-10 15:14:51 +0530

    added all-data support

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer, ProductCreateSerializer, ProductListSerializer
+from rest_framework.response import Response
 
 class ProductCreateView(generics.CreateAPIView):
     queryset = Product.objects.all()
@@ -14,6 +15,13 @@ class ProductListView(generics.ListAPIView):
 
     def get_serializer_context(self):
         return {'request': self.request}
+    
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get('all-data') == 'true':
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data, status=200)
+        return super().list(request, *args, **kwargs)
 
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
